48e0a2421d4ba4320bb0d09c4adfc022ba8f94b8
Umais Iqbal <malikumais777@gmail.com>
2025-08-06 13:33:17 +0500

    production-v2

--- src/config/auth0Config.ts
+++ src/config/auth0Config.ts
@@ -1,6 +1,6 @@
 export const auth0Config = {
   domain: "dev-425tsbqbsydrl817.us.auth0.com", // e.g., dev-xyz123.us.auth0.com
   clientId: "khOeXW1zhuOR8qAz4xHbOIhtWVsMWy9y",
-  redirectUri: "https://purrscribeai.netlify.app/dashboard",
+  redirectUri: "https://puurcribe-v2.netlify.app/dashboard",
   audience: "", // optional
 };
